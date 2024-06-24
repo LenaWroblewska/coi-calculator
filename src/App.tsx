ba3e870c6869fa2c8799b3ec062602bcd8000fea
Lena <lena.wrblwsk@gmail.com>
2024-06-24 12:17:08 +0100

    toolbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { MonthToolbar } from './Components/MonthToolbar';
+import { DataTable } from './Components/DataTable';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <DataTable />
+      <MonthToolbar />
     </div>
   );
 }
---
+++ src/Components/DataTable.tsx
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+
+export function DataTable() {
+    const people = ["Ben Apes", "Louie Franchino", "Matthew Beanland"];
+    const weeks = ["Week 1", "Week 2", "Week 3", "Week 4"];
+    return (
+        <div>
+        </div>
+    );
+};
